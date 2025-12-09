fc4326eeda95b44a85790bfc6ba73220314431b0
ShangYi7 <shangyi6.tw@gmail.com>
2025-12-09 21:05:14 +0800

    重構程式碼以改善可讀性，修正變數命名並刪除不必要的檔案

--- 10/main.py
+++ 10/main.py
@@ -1,15 +1,23 @@
 while True:
-    input = list(map(int, input().split()))
-    n = input[0]
+    nums = list(map(int, input().split()))
+    n = nums[0]
     if n == 0:
         break
-    data = input[1:]
+    data = nums[1:]
     data.sort()  # 升序排序
 
+    #中位數
     if n % 2 == 1:  # 奇數
         median = data[(n + 1) // 2 - 1]
         output = median
     else:  # 偶數
         median = data[n // 2 - 1]
         output = median
-
+    print(output,end=" ")
+    #變異數
+    ans = 0
+    mean = sum(data) // n #平均值 # sum == 全部加起來
+    for x in data:
+        ans += (x - mean) * (x - mean)
+    ans//=n
+    print(f"{ans:0.0f}")
